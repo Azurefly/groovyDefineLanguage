72e5ecb0baa46f55d7a3d68412da62d3db5f519f
Su payne <azure_dream@foxmail.com>
2026-09-15 15:52:14 +0800

    feat: add exchange boundary model

---
+++ gdl-runtime/src/main/java/com/pl/gdl/runtime/federation/ExchangeBoundary.java
@@ -0,0 +1,20 @@
+package com.pl.gdl.runtime.federation;
+
+import java.io.Serializable;
+
+public record ExchangeBoundary(
+        String id,
+        String sourceNodeId,
+        String targetNodeId,
+        String sourceFragmentId,
+        String targetFragmentId,
+        ExecutionDomain sourceDomain,
+        ExecutionDomain targetDomain,
+        MaterializationMode mode
+) implements Serializable {
+    public enum MaterializationMode {
+        MEMORY,
+        INTERMEDIATE_TABLE,
+        REMOTE_DRIFT
+    }
+}
