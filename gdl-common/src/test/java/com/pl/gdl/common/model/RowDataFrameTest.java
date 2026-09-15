32c04239e316dd89be2ced304999c6e05f69f71f
Su payne <azure_dream@foxmail.com>
2026-09-15 09:00:15 +0800

    test(common): cover dataframe iterator immutability

--- gdl-common/src/test/java/com/pl/gdl/common/model/RowDataFrameTest.java
+++ gdl-common/src/test/java/com/pl/gdl/common/model/RowDataFrameTest.java
@@ -3,9 +3,11 @@ package com.pl.gdl.common.model;
 import com.pl.gdl.common.enums.DataType;
 import org.junit.jupiter.api.Test;
 
+import java.util.Iterator;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class RowDataFrameTest {
 
@@ -33,6 +35,18 @@ public class RowDataFrameTest {
             assertThat((Object) row.getValue("id")).isNotNull();
         }
         assertThat(count).isEqualTo(2);
+    }
+
+    @Test
+    public void testIteratorCannotRemoveInternalRows() {
+        RowDataFrame df = new RowDataFrame(List.of(new ColumnInfo("id", "int")));
+        df.addRowValue(List.of(1));
+
+        Iterator<Row> iterator = df.iterator();
+        assertThat(iterator.next().getValue("id")).isEqualTo(1);
+        assertThatThrownBy(iterator::remove)
+                .isInstanceOf(UnsupportedOperationException.class);
+        assertThat(df.rowSize()).isEqualTo(1);
     }
 
     @Test
